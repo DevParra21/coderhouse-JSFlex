67f52a1539cdb54148a0d3f75cfcef29e1400bb4
Patricio Escudero <edparra@outlook.com>
2024-12-15 23:52:06 -0600

    simplifacion de for's deacuerdo a lo visto

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,128 +1,155 @@
-window.onload = (event) => {
-    /* Objetos literales */
 
-    //Clinicas
-    const ClinicaDefault ={
-        id:0,
-        nombre:"Seleccione una Clínica...",
-        urlDireccion:""
-    }
+/* Objetos literales */
 
-    const Clinica1 = {
-        id : 1,
-        nombre:"Pethouse Caprove",
-        urlDireccion:"https://maps.app.goo.gl/GTi9YhjZpfxsiBhs8"
-    }
+//Clinicas
+const ClinicaDefault ={
+    id:0,
+    nombre:"Seleccione una Clínica...",
+    urlDireccion:""
+}
 
-    const Clinica2 = {
-        id:2,
-        nombre:"Veterinaria Panda",
-        urlDireccion:"https://maps.app.goo.gl/hbNtE7i7wExRxbt47"
-    }
+const Clinica1 = {
+    id : 1,
+    nombre:"Pethouse Caprove",
+    urlDireccion:"https://maps.app.goo.gl/GTi9YhjZpfxsiBhs8"
+}
 
-    const Clinica3={
-        id:3,
-        nombre:"Veterinaria ANIMED",
-        urlDireccion: "https://maps.app.goo.gl/6SDsiu4TTiezH7ov7"
-    }
+const Clinica2 = {
+    id:2,
+    nombre:"Veterinaria Panda",
+    urlDireccion:"https://maps.app.goo.gl/hbNtE7i7wExRxbt47"
+}
 
-    //Servicios
-    const Servicio1 ={
-        id:1,
-        nombre:"Baño de Mascota",
-        precio:19.99
-    }
+const Clinica3={
+    id:3,
+    nombre:"Veterinaria ANIMED",
+    urlDireccion: "https://maps.app.goo.gl/6SDsiu4TTiezH7ov7"
+}
 
-    const Servicio2 = {
-        id:2,
-        nombre:"Desparacitación",
-        precio:56.99
-    }
+//Servicios
+const Servicio1 ={
+    id:1,
+    nombre:"Baño de Mascota",
+    precio:19.99
+}
 
-    const Servicio3 = {
-        id:3,
-        nombre:"Vacunación",
-        precio:105.69
-    }
+const Servicio2 = {
+    id:2,
+    nombre:"Desparacitación",
+    precio:56.99
+}
 
-    /* Arreglos */
-    //Clinicas
-    const Clinicas=[ClinicaDefault,Clinica1,Clinica2,Clinica3]
+const Servicio3 = {
+    id:3,
+    nombre:"Vacunación",
+    precio:105.69
+}
 
-    //Servicios
-    const Servicios=[Servicio1,Servicio2,Servicio3]
+//Reservaciones
+const Reservacion1 ={
+    citaID:1,
+    nombreMascota:"Shadow",
+    nombreClinica:"Pethouse Caprove",
+    urlClinica:"https://maps.app.goo.gl/juX2atSGG9qgB6Tv5",
+    fecha:"01/01/2025",
+    hora:"11:30"
+}
+const Reservacion2 ={
+    citaID:2,
+    nombreMascota:"Magna",
+    nombreClinica:"Veterinaria Panda",
+    urlClinica:"https://maps.app.goo.gl/juX2atSGG9qgB6Tv5",
+    fecha:"18/12/2024",
+    hora:"09:00"
+}
+const Reservacion3 ={
+    citaID:3,
+    nombreMascota:"Totti",
+    nombreClinica:"Veterinaria ANIMED",
+    urlClinica:"https://maps.app.goo.gl/juX2atSGG9qgB6Tv5",
+    fecha:"02/03/2025",
+    hora:"15:30"
+}
 
-    /* Variables globales */
-    let totalAmount = 0.00
+/* Arreglos */
+//Clinicas
+const Clinicas=[ClinicaDefault,Clinica1,Clinica2,Clinica3]
 
-    /*logica inicial*/
-    //Llenar dropdown de Clinicas disponibles
-    let cbClinicas = document.getElementById("cbClinicas")
-    Clinicas.forEach(element => {
-        let option = document.createElement("option")
-        option.textContent = element.nombre
-        option.value = element.id
-        cbClinicas.appendChild(option)
-    });
+//Servicios
+const Servicios=[Servicio1,Servicio2,Servicio3]
 
-    //Llenar dropdown de Servicios Disponibles
-    let cbServicios1 = document.getElementById("cbServicios1")
-    Servicios.forEach(element => {
-        let option = document.createElement("option")
-        option.textContent = element.nombre + " - $" + element.precio
-        option.value=element.precio
-        cbServicios1.appendChild(option)
-    });
+//Reservaciones
+const Reservaciones=[Reservacion1,Reservacion2,Reservacion3]
 
-    let cbServicios2 = document.getElementById("cbServicios2")
-    Servicios.forEach(element => {
-        let option = document.createElement("option")
-        option.textContent = element.nombre + " - $" + element.precio
-        option.value=element.precio
-        cbServicios2.appendChild(option)
-    });
+/* Variables globales */
+let totalAmount = 0.00
 
-    let cbServicios3 = document.getElementById("cbServicios3")
-    Servicios.forEach(element => {
-        let option = document.createElement("option")
-        option.textContent = element.nombre + " - $" + element.precio
-        option.value=element.precio
-        cbServicios3.appendChild(option)
-    });
+/*logica inicial*/
+//Llenar dropdown de Clinicas disponibles
+let cbClinicas = document.getElementById("cbClinicas")
+Clinicas.forEach(element => {
+    let option = document.createElement("option")
+    option.textContent = element.nombre
+    option.value = element.id
+    cbClinicas.appendChild(option)
+});
 
-    //Ocultar dropdown de servicios extras opcionales
-    let service2 = document.getElementById("servicio2")
-    service2.style.display="none"
+//Llenar dropdown de Servicios Disponibles
+let cbServicios1 = document.getElementById("cbServicios1")
+for(let i=0; i < Servicios.length; i++){
+    let option = document.createElement("option")
+    option.textContent = Servicios[i].nombre + " - $" + Servicios[i].precio
+    option.value=Servicios[i].precio
+    cbServicios1.appendChild(option)   
+}
 
-    let service3 = document.getElementById("servicio3")
-    service3.style.display ="none"
+let cbServicios2 = document.getElementById("cbServicios2")
+for(let i=0; i<Servicios.length; i++){
+    let option = document.createElement("option")
+    option.textContent = Servicios[i].nombre + " - $" + Servicios[i].precio
+    option.value=Servicios[i].precio
+    cbServicios2.appendChild(option)   
+}
 
-    //Inicializar monto total a ceros
-    let lblTotalAmount = document.getElementById("totalAmount")
-    lblTotalAmount.textContent = parseFloat(totalAmount).toFixed(2)
+let cbServicios3 = document.getElementById("cbServicios3")
+for(let i=0; i<Servicios.length; i++){
+    let option = document.createElement("option")
+    option.textContent = Servicios[i].nombre + " - $" + Servicios[i].precio
+    option.value=Servicios[i].precio
+    cbServicios3.appendChild(option)   
+}
 
-    //calcular costo total de servicios
-    cbServicios1.addEventListener("change", (event) =>{
-        lblTotalAmount.textContent = calcularMontoTotal(cbServicios1.value,cbServicios2.value,cbServicios3.value)
-    })
+//Ocultar dropdown de servicios extras opcionales
+let service2 = document.getElementById("servicio2")
+service2.style.display="none"
+
+let service3 = document.getElementById("servicio3")
+service3.style.display ="none"
+
+//Inicializar monto total a ceros
+let lblTotalAmount = document.getElementById("totalAmount")
+lblTotalAmount.textContent = parseFloat(totalAmount).toFixed(2)
+
+//calcular costo total de servicios
+cbServicios1.addEventListener("change", (event) =>{
+    lblTotalAmount.textContent = calcularMontoTotal(cbServicios1.value,cbServicios2.value,cbServicios3.value)
+})
+
+cbServicios2.addEventListener("change", (event) =>{
+    lblTotalAmount.textContent = calcularMontoTotal(cbServicios1.value,cbServicios2.value,cbServicios3.value)
+})
+
+cbServicios3.addEventListener("change", (event) =>{
+    lblTotalAmount.textContent = calcularMontoTotal(cbServicios1.value,cbServicios2.value,cbServicios3.value)
+})
+
+function calcularMontoTotal(firstServiceValue, SecondServiceValue, ThirdServiceValue){
     
-    cbServicios2.addEventListener("change", (event) =>{
-        lblTotalAmount.textContent = calcularMontoTotal(cbServicios1.value,cbServicios2.value,cbServicios3.value)
-    })
+    let sum = parseFloat(firstServiceValue) + parseFloat(SecondServiceValue)  + 
+    parseFloat(ThirdServiceValue)
     
-    cbServicios3.addEventListener("change", (event) =>{
-        lblTotalAmount.textContent = calcularMontoTotal(cbServicios1.value,cbServicios2.value,cbServicios3.value)
-    })
-
-    function calcularMontoTotal(firstServiceValue, SecondServiceValue, ThirdServiceValue){
-        console.log(firstServiceValue + " - " + SecondServiceValue + " - " +
-             ThirdServiceValue)
-        let sum = parseFloat(firstServiceValue) + parseFloat(SecondServiceValue)  + 
-        parseFloat(ThirdServiceValue)
-        
-        return sum.toFixed(2)
-    }
-};
+    return sum.toFixed(2)
+}
 
 //mostrar dropdowns de servicios extras
 function addService(){
@@ -138,4 +165,21 @@ function addService(){
 
     let btnServices = document.getElementById("btnService")
     btnServices.disabled = service3.style.display === "none" ? false : true
-};
+}
+
+//Llenar lista de Reservaciones
+let lsAppointments = document.getElementById("tableAppointments")
+lsAppointments.innerHTML=""
+let tableData=""
+for(let i=0; i < Reservaciones.length; i++){
+    tableData+="<tr>"
+    tableData+="<td>"+Reservaciones[i].citaID+"</td>"+"<td>"+Reservaciones[i].nombreMascota+
+    "</td>"+"<td><a href=\"" + Reservaciones[i].urlClinica + "\">"
+    +Reservaciones[i].nombreClinica+"</a></td>"+
+    "<td>"+Reservaciones[i].fecha+" "+Reservaciones[i].hora+"</td>"
+    tableData+="</tr>"
+}
+lsAppointments.innerHTML=tableData
+
+
+
